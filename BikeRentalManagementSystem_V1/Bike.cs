a24829c352bc5b782d3e97e37e57bd7022f3600b
Nithusha S. <UT01594@unicomtic.com>
2024-10-11 10:01:25 +0530

    first commit

---
+++ BikeRentalManagementSystem_V1/Bike.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRentalManagementSystem_V1
+{
+    internal class Bike
+    {
+        private string bikeId { get; set; }
+        private string brand { get; set; }
+        private string model { get; set; }
+        private decimal RentalPrice { get; set; }
+
+        public Bike(string bikeId, string brand, string model, decimal rentalPrice)
+        {
+            this.bikeId = bikeId;
+            this.brand = brand;
+            this.model = model;
+            RentalPrice = rentalPrice;
+        }
+        public override string ToString()
+        {
+            return $"bikeid:{bikeId},brand:{brand},model:{model},rentalprice:{RentalPrice}";
+        }
+    }
+}
